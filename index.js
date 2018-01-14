a14cba4f62b5141cf73afdb261de81f1c68fc45d
BeeCoding <rafael@beecoding.net>
2018-01-14 03:46:09 -0200

    fix port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const request = require('request')
 const { forEach } = require('p-iteration')
 const path = require('path')
-const PORT = 5011
+const PORT = process.env.PORT || 5000
 
 var online = "<span style='color: green'>Online</span>";
 var offline = "<span style='color: red'>Offline</span>";
