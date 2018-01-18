6606d0c643bfa6bd82ca2754b2ae0556817c4ea9
BeeCoding <rafael@beecoding.net>
2018-01-18 02:12:58 -0200

    now it supports more types of outage...

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const path = require('path');
 const request = require('request');
 const status = require('./status.js');
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
+const PORT = 5011;
 
 status.start();
 
--- status.js
+++ status.js
@@ -7,12 +7,12 @@ var offline = "<span style='color: " + "#ff7070" + "'>Offline</span>";
 var status = [];
 var ready = false;
 
-var servers = [["https://fivem.net/", "Main Webserver"], ["https://servers.fivem.net/", "Server List"], ["https://servers-live.fivem.net/", "Server API", (e, r, b) => {return(!e && !b.includes("Error") && !b.includes("a padding to disable MSIE and Chrome friendly error page"))}], ["https://runtime.fivem.net/", "Runtime"], ["https://metrics.fivem.net/", "Metrics", (e, r, b) => {return(!e && b.includes("Matomo"))}], ["https://forum.fivem.net/", "Forums", (e, r, b) => {return(!e && b.includes("hidden-login-form"))}], ["https://wiki.fivem.net/", "Wiki"], ["https://keymaster.fivem.net/", "Keymaster Server"], ["https://lambda.fivem.net/", "Lambda"], ["http://crashes.fivem.net/", "Crash server"]];
+var servers = [["https://fivem.net/", "Main Webserver"], ["https://servers.fivem.net/", "Server List"], ["https://servers-live.fivem.net/api/servers/proto", "Server API"], ["https://runtime.fivem.net/", "Runtime"], ["https://metrics.fivem.net/", "Metrics", (e, r, b) => {return(!e && b.includes("Matomo"))}], ["https://forum.fivem.net/", "Forums", (e, r, b) => {return(!e && b.includes("hidden-login-form"))}], ["https://wiki.fivem.net/", "Wiki"], ["https://keymaster.fivem.net/", "Keymaster Server"], ["https://lambda.fivem.net/", "Lambda"], ["http://crashes.fivem.net/", "Crash server"]];
 
 function poll() {
   tmpstatus = [];
   forEach(servers, async (server, i) => {
-    await request(server[0], function (error, response, body) {
+    request(server[0], {timeout: 5000}, function (error, response, body) {
       if (server[2]) {
         if (server[2](error, response, body)) {
           tmpstatus.push([server[1], online, i, server[0]]);
@@ -20,7 +20,7 @@ function poll() {
           tmpstatus.push([server[1], offline, i, server[0]]);
         }
       } else {
-        if (!error && !body.includes("Error")) {
+        if (!error && !body.includes("Error") && (response.statusCode == 200 || response.statusCode == 404)) {
           tmpstatus.push([server[1], online, i, server[0]]);
         } else {
           tmpstatus.push([server[1], offline, i, server[0]]);
