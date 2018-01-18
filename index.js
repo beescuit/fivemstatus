6c9f4a1c5e3699ca9bbe296a938723e09363ece6
BeeCoding <rafael@beecoding.net>
2018-01-18 02:14:08 -0200

    forgot about the port... AGAIN

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const express = require('express');
 const path = require('path');
 const request = require('request');
 const status = require('./status.js');
-// const PORT = process.env.PORT || 5000;
-const PORT = 5011;
+const PORT = process.env.PORT || 5000;
 
 status.start();
 
