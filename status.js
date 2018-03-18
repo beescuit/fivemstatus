acaf48436faf32e94d17d1ad47327adfe3b1f100
BeeCoding <rafael@beecoding.net>
2018-03-17 22:43:21 -0300

    Remove crashes server

--- status.js
+++ status.js
@@ -4,16 +4,16 @@ const { forEach } = require('p-iteration');
 var status = [];
 var ready = false;
 
-var servers = [["https://fivem.net/", "Main Webserver"], ["https://servers.fivem.net/", "Server List"], ["https://servers-live.fivem.net/api/servers/proto", "Server API"], ["https://runtime.fivem.net/", "Runtime"], ["https://metrics.fivem.net/", "Metrics"], ["https://forum.fivem.net/", "Forums"], ["https://wiki.fivem.net/", "Wiki"], ["https://keymaster.fivem.net/", "Keymaster Server"], ["https://lambda.fivem.net/", "Lambda"], ["http://crashes.fivem.net/", "Crash server"]];
+var servers = [["https://fivem.net/", "Main Webserver"], ["https://servers.fivem.net/", "Server List"], ["https://servers-live.fivem.net/api/servers/proto", "Server API"], ["https://runtime.fivem.net/", "Runtime"], ["https://metrics.fivem.net/", "Metrics"], ["https://forum.fivem.net/", "Forums"], ["https://wiki.fivem.net/", "Wiki"], ["https://keymaster.fivem.net/", "Keymaster Server"], ["https://lambda.fivem.net/", "Lambda"]];
 
 function poll() {
   tmpstatus = [];
   forEach(servers, async (server, i) => {
     request(server[0], {timeout: 5000, time : true}, function (error, response) {
       if (!error && (response.statusCode == 200 || response.statusCode == 404)) {
-        tmpstatus.push([server[1], "<span style='color: " + "#70ff8a" + "'>Online (" + response.elapsedTime + "ms)</span>", i]);
+        tmpstatus.push([server[1], `<span style='color: #70ff8a'>Online (${response.elapsedTime}ms</span>` , i]);
       } else {
-        tmpstatus.push([server[1], "<span style='color: " + "#ff7070" + "'>Offline</span>", i]);
+        tmpstatus.push([server[1], `<span style='color: #ff7070'>Offline</span>`, i]);
       }
       if (tmpstatus.length == servers.length) {
         tmpstatus = tmpstatus.sort((a, b) => {
