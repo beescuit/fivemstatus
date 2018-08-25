711d91ed115b350043e05b77e4995d402a5fda49
BeeCoding <rafael@tunning.com.br>
2018-08-25 00:31:26 -0300

    Fix verification on lambda.fivem.net

--- status.js
+++ status.js
@@ -6,7 +6,7 @@ var servers = require('./servers.json')
 
 async function poll() {
   status = await Promise.all(servers.map(async ({url, name}, i) => {
-    var response = await rp.head({url, resolveWithFullResponse: true, timeout: 5000, time : true}).catch(e => e)
+    var response = await rp.head({url, simple: false, resolveWithFullResponse: true, timeout: 5000, time : true}).catch(e => e)
     var online = (response.statusCode == 200 || response.statusCode == 404) ? response.elapsedTime : false
     return {url, name, online}
   }))
