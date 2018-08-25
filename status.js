426561ea014351182a72a4065fcebdedd1ca011d
BeeCoding <rafael@tunning.com.br>
2018-08-25 13:24:51 -0300

    More code style magic

--- status.js
+++ status.js
@@ -1,12 +1,12 @@
 const rp = require('request-promise-native')
 
-var status = []
-var servers = require('./servers.json')
+let status = []
+let servers = require('./servers.json')
 
 async function poll () {
   status = await Promise.all(servers.map(async ({url, name}) => {
-    var response = await rp.head({url, simple: false, resolveWithFullResponse: true, timeout: 5000, time: true}).catch(e => e)
-    var online = (response.statusCode === 200 || response.statusCode === 404) ? response.elapsedTime : false
+    let response = await rp.head({url, simple: false, resolveWithFullResponse: true, timeout: 5000, time: true}).catch(e => e)
+    let online = (response.statusCode === 200 || response.statusCode === 404) ? response.elapsedTime : false
     return {url, name, online}
   }))
 }
